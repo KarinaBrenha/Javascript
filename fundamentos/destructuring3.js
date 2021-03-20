050de05cacdef7438bcde7ad762ce653d5d067f3
KarinaBrenha <karinabrenha0@gmail.com>
2021-03-20 14:48:06 -0300

    corrigindo comentario

--- fundamentos/destructuring3.js
+++ fundamentos/destructuring3.js
@@ -5,6 +5,6 @@ function rand({min = 0, max = 1000}) {
 
 const obj = {max: 50, min: 40}
 console.log(rand(obj))
-console.log(rand({min: 955})) // da valores randomicos, aleaorios
+console.log(rand({min: 955})) // da valores randomicos, aleatorios
 console.log(rand({}))
 console.log(rand()) // da erro porque nao foi definido
