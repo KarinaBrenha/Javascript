cf64e72d806eccf6791adfcf78404f4d0dd793c2
KarinaBrenha <karinabrenha0@gmail.com>
2021-03-24 22:03:30 -0300

    aula if e else

---
+++ controle/ifElse.js
@@ -0,0 +1,11 @@
+imprimirResultado = function(nota) {
+    if(nota >= 7) {
+        console.log('Aprovado!')
+    } else {
+        console.log('Reprovado!')
+    }
+}
+
+imprimirResultado(10)
+imprimirResultado(4)
+imprimirResultado('Epa!')  // nesse caso é falso, cuidado pois js é fracamente tipado
