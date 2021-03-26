892391668f9592067ef4f5f8881a1aa72ed36e85
KarinaBrenha <karinabrenha0@gmail.com>
2021-03-26 17:01:58 -0300

    switch

---
+++ controle/switch.js
@@ -0,0 +1,27 @@
+const imprimirResultado = function (nota) {
+    switch (Math.floor(nota)) {
+        case 10:
+        case 9: 
+            console.log('Quadro de honra')
+            break   // só sai do switch quando colocar break
+        case 8:
+        case 7:
+            console.log('Aprovado')
+            break
+        case 6: case 5: case 4:
+            console.log('Recuperação')
+            break
+        case 3: case 2: case 1: case 0:
+            console.log('Reprovado')
+            break
+        default:    // tem a mesma lógica de else
+            console.log('Nota inválida')
+    }
+}
+
+imprimirResultado(10)
+imprimirResultado(8.9)
+imprimirResultado(6.55)
+imprimirResultado(2.3)
+imprimirResultado(-1)
+imprimirResultado(11)
