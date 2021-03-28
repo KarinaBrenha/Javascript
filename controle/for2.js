42981ac2c9955567aef197998a9fa64a300403a6
KarinaBrenha <karinabrenha0@gmail.com>
2021-03-28 14:57:59 -0300

    aula for in

---
+++ controle/for2.js
@@ -0,0 +1,17 @@
+const notas = [6.7, 7.6, 9.8, 8.1, 7.7]
+
+for (let i in notas){
+    console.log(i, notas[i])
+}
+
+const pessoa = {
+    nome: 'Ana',
+    sobrenome: 'Silva',
+    idde: 29,
+    peso: 64
+}
+
+for (let atributo in pessoa) {
+    console.log(`${atributo} = ${(pessoa[atributo])}`)
+}
+
