424e1feeacd96ce923a0390b79315076effd7a99
KarinaBrenha <karinabrenha0@gmail.com>
2021-04-02 16:31:36 -0300

    closure

---
+++ funçao/closure.js
@@ -0,0 +1,17 @@
+// closure é o escopo riado qaundo uma função é declarada
+// esse escopo permite a função acessar e manipular variáveis externas à função
+
+// contexto léxico em ação!
+
+const x ='Global'
+
+function fora() {
+    const x = 'Local'
+    function dentro() {
+        return x
+    }
+    return dentro
+}
+
+const minhaFuncao = fora()
+console.log(minhaFuncao())
