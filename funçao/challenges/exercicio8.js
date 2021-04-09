c36b490367037767c18f3c63a9b091786eefcadb
KarinaBrenha <karinabrenha0@gmail.com>
2021-04-08 21:20:03 -0300

    exercicio 8 ajuste

--- funçao/challenges/exercicio8.js
+++ funçao/challenges/exercicio8.js
@@ -3,39 +3,65 @@ registro de todos os as pontuações feitas por jogo. Após cada jogo ele anota 
 mesmo é maior ou menor que seu melhor e pior desempenho. Dada uma lista string = “pontuação1 pontuação2
 pontuação3 etc..”, escreva uma função que ao recebê-la irá comparar os valores um a um e irá retornar um
 vetor com o número de vezes que ele bateu seu recorde de maior número de pontos e quando fez seu pior
-jogo. (Número do pior jogo). */
+jogo. (Número do pior jogo). Obs.: O primeiro jogo não conta como novo recorde do melhor.
+Exemplo:
+String: “10 20 20 8 25 3 0 30 1”
+Retorno: [3, 7] (Significa que ele bateu três vezes seu recorde de melhor pontuação e a pior pontuação
+aconteceu no sétimo jogo.)*/
 
-const calcularPontuacao = lista => {
-    const arrayPontuacao = lista.split(' ');
-    let piorPontuacao = 0;
-    let novaPontuacao = 0;
-    let melhorPontuacao = 0;
-    let recorde = 0;
-    for (let i = 0; i < arrayPontuacao.length; i++){
-        novaPontuacao = Number(arrayPontuacao[i]);
-        if (i === 0){
-            piorPontuacao = novaPontuacao;
-            melhorPontuacao = novaPontuacao;
+const calcularJogo = listaPont => {
+    const arraylista = listaPont.split(' ');
+    let pontNova;
+    let pontPior;
+    let pontMelhor;
+    let piorJogada;
+    let pontRecorde = 0;
+    for (let i = 0; i < arraylista.length; i++) {
+        pontNova = Number(arraylista[i]);
+        if ( i === 0) {
+            pontMelhor = pontNova;
+            pontPior = pontNova;            
         }
-        if (novaPontuacao > melhorPontuacao && i !== 0) {
-            melhorPontuacao = novaPontuacao;
-            recorde++;
+        if (pontNova > pontMelhor) {
+            pontMelhor = pontNova;
+            pontRecorde++
         }
-        if (novaPontuacao < piorPontuacao) {
-            piorPontuacao = novaPontuacao;
+        if (pontNova < pontPior) {
+            pontPior = pontNova;
+            piorJogada = i + 1;            
+        }
+        if ( i === 0 && pontNova === 0){
+            piorJogada = 1;
         }
     }
-    return [recorde, piorPontuacao];
+    return [pontRecorde, piorJogada];
 }
 
-console.log(calcularPontuacao("10 5 7 30 27 50"));
 
-
-
-
-
-
-
+// const calcularPontuacao = lista => {
+//     let arrayList = lista.split(' ');
+//     arrayList = arrayList.map(item => Number(item));
+//     let piorJogo = Infinity;
+//     let indexPiorJogo = null;
+//     let recordes = 0;
+//     let melhorPontuação;
+//     for(let i = 0; i < arrayList.length; i++){
+//       if(i === 0) {
+//         melhorPontuação = arrayList[0];
+//       }
+  
+//       if(arrayList[i] < piorJogo){
+//         piorJogo = arrayList[i];
+//         indexPiorJogo = i + 1;
+//       }
+//       if(i !== 0 && arrayList[i] > melhorPontuação) {
+//         recordes++;
+//       }
+//     }
+//     return [recordes, indexPiorJogo]
+//   }
+  
+  console.log(calcularJogo("10 20 20 8 25 3 0 30 1"));
 
 
 
