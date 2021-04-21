61a179251d9243c77ea59e75084ce0faf124d0b1
KarinaBrenha <karinabrenha0@gmail.com>
2021-04-20 21:11:07 -0300

    herança 4

--- objeto/heranca3.js
+++ objeto/heranca3.js
@@ -5,7 +5,7 @@ filha1.nome = 'Ana';
 console.log(filha1.corCabelo);
 
 const filha2 = Object.create(pai, {
-    nome: { value: 'Bia', writable: false, enumerable: true}
+    nome: { value: 'Bia', writable: false, enumerable: true};
 })
 
 console.log(filha2.nome);
