033155b152c064fdd1ade39a37737e80f8f6f682
KarinaBrenha <karinabrenha0@gmail.com>
2021-04-26 20:03:05 -0300

    map 1

---
+++ array/map1.js
@@ -0,0 +1,16 @@
+const nums = [1, 2, 3, 4, 5];
+
+// for com propódito
+let resultado = nums.map(function(elemento) {
+    return elemento * 2;
+})
+
+console.log(resultado, nums);
+
+const soma10 = elemento => elemento + 10;
+const triplo = elemento => elemento * 3;
+const paraDinheiro = elemento => `R$ ${parseFloat(elemento).toFixed(2).replace('.', ',')}`;
+
+resultado = nums.map(soma10).map(triplo).map(paraDinheiro); // foi transformado pelas 3 funções
+console.log(resultado);
+
