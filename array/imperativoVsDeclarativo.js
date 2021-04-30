02b81c9d0944e2e73658c0b09b321acadb99a4b9
KarinaBrenha <karinabrenha0@gmail.com>
2021-04-29 22:24:15 -0300

    imperativo vs declarativo

--- array/imperativoVsDeclarativo.js
+++ array/imperativoVsDeclarativo.js
@@ -15,3 +15,5 @@ const getNota = aluno => aluno.nota;
 const soma = (total, atual) => total + atual;
 const total2 = alunos.map(getNota).reduce(soma);
 console.log(total2 / alunos.length);
+
+// select codigo, nome, email from clientes where ativo = 1 (declarativo)
