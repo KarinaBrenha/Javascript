1bc53f8753037df4eabb37c5e4568aaa58870e56
KarinaBrenha <karinabrenha0@gmail.com>
2021-05-04 20:35:38 -0300

    exercicio 5

---
+++ challenges/exercicio5.js
@@ -0,0 +1,7 @@
+// Crie uma função que receba dois números e retorne se o primeiro é maior ou igual ao segundo.
+
+const maiorOuIgual = (numero1, numero2) => numero1 >= numero2;
+
+console.log(maiorOuIgual(4, 8));
+console.log(maiorOuIgual(59, 8));
+console.log(maiorOuIgual(8, 8));
