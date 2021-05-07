a058eaaf7ba09f48b6343078c369a0a898da6052
KarinaBrenha <karinabrenha0@gmail.com>
2021-05-06 21:49:11 -0300

    exercicio 14

--- challenges/exercicio14.js
+++ challenges/exercicio14.js
@@ -13,11 +13,16 @@ preco: 12000
 
 function objetoParaArray(objeto) {
     const chaves = Object.keys(objeto)
+    console.log(Object.keys(objeto))
     const resultado = chaves.map( chave => [chave, objeto[chave]] )
     return resultado
     }
 
-    
+// outra forma
+function objetoParaArray(objeto) {
+    return Object.entries(objeto)
+    }
+
 console.log(objetoParaArray({
     nome: "Maria",
     profissao: "Desenvolvedora de software"
