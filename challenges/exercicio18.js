c6e435f9c03c6012b19055fd9c9f8409aab12d63
KarinaBrenha <karinabrenha0@gmail.com>
2021-05-07 22:47:16 -0300

    exercicio 18

---
+++ challenges/exercicio18.js
@@ -0,0 +1,24 @@
+/* Você está trabalhando numa aplicação pessoal de controle de despesas. Na tela principal dessa aplicação, é
+possível adicionar produtos ou serviços, informando nome, categoria e preço. Uma funcionalidade que você
+está desenvolvendo no momento é a de somar o total das despesas.
+Crie uma função que receba um array de produtos e retorne o total das despesas.
+Exemplos:
+despesasTotais([
+{nome: "Jornal online", categoria: "Informação", preco: 89.99},
+{nome: "Cinema", categoria: "Entretenimento", preco: 150}
+]) // retornará 239.99
+despesasTotais([
+{nome: "Galaxy S20", categoria: "Eletrônicos", preco: 3599.99},
+{nome: "Macbook Pro", categoria: "Eletrônicos", preco: 30999.90}
+]) // retornará 34599.89 */
+
+function calcularDespesas(produtos) {
+    const somarValores = (acumulador, preco) => acumulador + preco;
+    const verificarPrecos = produto => produto.preco;
+    return produtos.map(verificarPrecos).reduce(somarValores);
+}   
+
+
+
+console.log(calcularDespesas([{nome: "Galaxy S20", categoria: "Eletrônicos", preco: 3599.99},
+{nome: "Macbook Pro", categoria: "Eletrônicos", preco: 30999.90}]));
