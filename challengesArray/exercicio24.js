ddf8df32e337625a4e629d8aede4baf09ddf6687
KarinaBrenha <karinabrenha0@gmail.com>
2021-05-12 21:34:53 -0300

    exercicio 24

---
+++ challengesArray/exercicio24.js
@@ -0,0 +1,17 @@
+/* Desenvolva uma função que recebe um caractere e uma string como parâmetros e retorne a quantidade de
+vezes que o caractere se repete na string. A função deverá ser case-sensitive, ou seja, irá diferenciar
+maiúsculas de minúsculas.
+Exemplos:
+contarCaractere("r", "A sorte favorece os audazes") // retornará 2
+contarCaractere("c", "Conhece-te a ti mesmo") // retornará 1 */
+
+const contarCaractere = (letra, frase) => {
+    let quantidade = 0;
+    for (let i = 0; i < frase.length; i++) {
+        if ( letra === frase[i]) quantidade++;
+    }
+    return quantidade;
+}
+
+console.log(contarCaractere("r", "A sorte favorece os audazes"));
+console.log(contarCaractere("c", "Conhece-te a ti mesmo"));
