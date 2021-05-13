fc73578b6760cd95d9da2d994935e5429c0cd623
KarinaBrenha <karinabrenha0@gmail.com>
2021-05-12 22:18:30 -0300

    exercicio 27

---
+++ challengesArray/exercicio27.js
@@ -0,0 +1,19 @@
+/* Desenvolva uma função que recebe um objeto como parâmetro e retorne um outro objeto que corresponde ao
+ao objeto recebido como parâmetro, porém com as posições das chaves e valores invertidas, conforme
+exemplo a seguir:
+Exemplo:
+inverter({ a: 1, b: 2, c: 3}) // retornará { 1: "a", 2: "b", 3: "c"} */
+
+
+function inverter(objeto) {
+    const objetoInvertido = {}
+    Object.entries(objeto).forEach( parChaveValor => {
+    const chave = 0,
+    valor = 1
+    objetoInvertido[ parChaveValor[valor] ] = parChaveValor[chave]
+    })
+    return objetoInvertido
+    }
+
+
+console.log(inverter({ a: 1, b: 2, c: 3}));
