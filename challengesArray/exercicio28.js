6219f7d057f549f0ec2a1a8aee51b62201118925
KarinaBrenha <karinabrenha0@gmail.com>
2021-05-22 21:48:36 -0300

    exercicio 28

--- challengesArray/exercicio28.js
+++ challengesArray/exercicio28.js
@@ -4,3 +4,10 @@ têm a quantidade de dígitos indicada pelo segundo parâmetro.
 Exemplos:
 filtrarPorQuantidadeDeDigitos([38, 2, 365, 10, 125, 11], 2) // retornará [38, 10, 11]
 filtrarPorQuantidadeDeDigitos([5, 9, 1, 125, 11], 1) // retornará [5, 9, 1] */
+
+const filtrarPorQuantidadeDeDigitos = (numeros, digito) => {
+    return numeros.filter(numero => String(numero).length === digito).map(numero => Number(numero)); 
+}
+
+console.log(filtrarPorQuantidadeDeDigitos([38, 2, 365, 10, 125, 11], 2));
+console.log(filtrarPorQuantidadeDeDigitos([5, 9, 1, 125, 11], 1));
