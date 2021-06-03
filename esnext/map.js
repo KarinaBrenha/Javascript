bc4086b926e179bd5a1feeaf53a1f18c38c05bae
KarinaBrenha <karinabrenha0@gmail.com>
2021-06-03 14:23:13 -0300

    map

--- esnext/map.js
+++ esnext/map.js
@@ -2,8 +2,8 @@ const tecnologias = new Map();
 tecnologias.set('react', { framework: false});
 tecnologias.set('angular', { framework: true});
 
-console.log(tecnologias.react);
-console.log(tecnologias.get('react').framework);
+console.log(tecnologias.react); // nao encontra
+console.log(tecnologias.get('react').framework); // jeito certo
 
 const chavesVariadas = new Map([
     [function () { }, 'função'],
@@ -15,10 +15,10 @@ chavesVariadas.forEach((vl, ch) => {
     console.log(ch, vl);
 })
 
-console.log(chavesVariadas.has(123));
+console.log(chavesVariadas.has(123)); // diz se essta contido ou nao no map
 chavesVariadas.delete(123);
-console.log(chavesVariadas.has(123));
-console.log(chavesVariadas.size);
+console.log(chavesVariadas.has(123)); // falso pois deletou o elemento
+console.log(chavesVariadas.size); // pra saber quantos elementos estao contidos dentro do map
 
 chavesVariadas.set(123, 'a');
 chavesVariadas.set(123, 'b');
